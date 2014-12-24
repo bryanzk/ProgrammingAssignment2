a68738f1efebb95d448dd0ef18e500a947261214
bryanzk <bryanzk@gmail.com>
2014-12-24 15:51:30 +0800

    add ... parameter processing

--- cachematrix.R
+++ cachematrix.R
@@ -20,14 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Before compute the inverse of matrix x, see if x has it cached , and return that if do.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     m <- x$getInverse()
     if (!is.null(m)) {
         message("getting cached data")
         return (m)
     }
     data <- x$get()
-    m <- solve(data)
+    m <- solve(data, ...)
     x$setInverse(m)
     m
 }
